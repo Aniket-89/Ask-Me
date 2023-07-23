1d9ecddb6afc00a6e9c4e964aee2a2472104cb65
Aniket <aniketkumarg99@gmail.com>
2023-07-23 16:20:33 +0530

    created templates dir and index.html

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.urls import reverse
+from django.db.models import Q
 
 # Create your views here.
+def index_view(request, *args, **kwargs):
+    return render(request, 'core/index.html', {})
